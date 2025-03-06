b2f15e703136653aa85e8b6d2db950f8ab2bbcc8
gitSouvik <csouvik01@gmail.com>
2025-03-06 14:54:10 +0530

    Removed number of files argument

--- create.sh
+++ create.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
 # Check if there are enough arguments
-if [ "$#" -lt 3 ]; then
-    echo "Usage: $0 <number_of_files> <filename1> <filename2> ... <file_extension>"
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <filename1> <filename2> ... <file_extension>"
     exit 1
 fi
 
-# Get the number of files from the first argument
-n=$1
-shift # Remove the first argument (number of files) from the list
-
 # Get the last argument as the file extension
 extension=${!#}
-set -- "${@:1:$(($# - 1))}" # Set all but the last argument as filenames
+
+# Set all but the last argument as filenames
+filenames=("${@:1:$(($# - 1))}")
 
 # Create files with specified names
-for name in "$@"; do
+for name in "${filenames[@]}"; do
     touch "${name}.${extension}"
+    echo "Created ${name}.${extension}"
 done
-
-echo "$n files created with the specified names and the extension '$extension'"
